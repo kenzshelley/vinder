3ed8ccaf7698cda7399c8dee78c2a3d71bd08d53
Mackenzie Shelley <kenzshelley@gmail.com>
2014-09-06 03:13:53 -0400

    Connected node server to firebase so that s3 mp3 url can be uploaded

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var $ = require('jquery'),
-    XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-$.support.cors = true;
-$.ajaxSettings.xhr = function() {
-  return new XMLHttpRequest();
-}
-
+var Firebase = require('firebase');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/recieve_mp3', function(req, res) {
-  
+  var url = '';
+  var fb = new Firebase('https://vinder.firebaseio.com/');
+  var post_user = fb.child('users');
+  post_user.push({
+    mp3_url: url
+  }); 
 
-  res.send("test");
+  res.send('Added mp3 url to firebase.');
 });
 
 app.listen(5000);
 console.log('Listening on port 3000...');
+
