1380bdc66fc00e6599c80940415baf900645ba8f
Matthew Grossman <matt@mrgrossman.com>
2014-09-06 02:52:54 -0400

    cleaned up some unused imports

--- mobile/android/vinder/app/src/main/java/vinder/vinder/VoiceRecordActivity.java
+++ mobile/android/vinder/app/src/main/java/vinder/vinder/VoiceRecordActivity.java
@@ -13,17 +13,9 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import org.apache.http.Header;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import com.loopj.android.http.*;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 
 public class VoiceRecordActivity extends Activity {
     private Button mRecordButton;
