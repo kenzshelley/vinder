944ba95e8d8a3e087cf8bd210bdef33a476abd3f
Mackenzie Shelley <kenzshelley@gmail.com>
2014-09-06 00:07:26 -0400

    Shitty attempt to access firebase.

--- server/server.py
+++ server/server.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from firebase import firebase
 app = Flask(__name__)
 
 @app.route("/")
 def main(): 
-  return "Nothing"
+  print "yo"
+  firebase = firebase.FirebaseApplication('https://vinder.firebaseio.com', None)
+  print "hi"
+  result = firebase.get('/Test', None)
+  return result
+#  return "Nothing"
 
 if __name__ == "__main__":
   app.run()
