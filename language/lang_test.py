d44079530a75fd485fa1997fa3d3a80f2b42e587
yichuanw <yichuanwang95@gmail.com>
2014-09-06 01:11:08 -0400

    nltk_features provides the functions and lang_test is a way to test the function in the command line

---
+++ language/lang_test.py
@@ -0,0 +1,9 @@
+from nltk_features import get_features 
+import sys
+
+# When running it stepwise I will just type in a few example sentences.
+
+print sys.argv
+test_text = sys.argv[1]
+test_features = get_features(test_text)
+print(test_features)
