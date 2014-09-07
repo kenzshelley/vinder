f661370c72cca0f13084effdb5ea0aadc1b49cc2
Mackenzie Shelley <kenzshelley@gmail.com>
2014-09-07 01:33:32 -0400

    functional node server

--- server/python/process_audio.py
+++ server/python/process_audio.py
@@ -5,20 +5,12 @@ from firebase import firebase
 from language import match
 
 
-def main(user_hash, mp3_url, audio_path): 
+def main(audio_path): 
   print 'running main'
 #  features = process_audio(audio_path)
   features = [1,2,3]
-  print "HELO"
   print features
-  # Store feature vector in Firebase
-  fb = firebase.FirebaseApplication('https://vinder.firebaseio.com', None)
-  fb.post('users/', {'anid': {'features': features,
-                     'user_hash': user_hash,
-                     'mp3_url': mp3_url,
-                     'matches': []}})
-  match.update_matches(user_hash)
 
 
 if __name__ == '__main__':
-  main(sys.argv[1], sys.argv[2], sys.argv[3])
+  main(sys.argv[1])
